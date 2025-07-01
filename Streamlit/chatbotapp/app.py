9996ac93ebcace942a17a478a79707f681e608d8
Praveen kumar <yenugulapraveen@gmail.com>
2025-07-01 12:39:55 +0530

    last code is lubricated soo updated new code

--- Streamlit/chatbotapp/app.py
+++ Streamlit/chatbotapp/app.py
@@ -1,23 +1,26 @@
-from langchain.llms import Cohere
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
 from dotenv import load_dotenv
 import os
+from langchain_cohere import ChatCohere
 
 
+load_dotenv()
+cohere_key = os.getenv("COHERE_API_KEY")
+
+def get_current_temperature(location: str) -> float:
+    """Used to get current temperature in a given location."""
+    return 53.3
 
 
 def ai_response(user_msg):
-    load_dotenv()
-    cohere_key = os.getenv("COHERE_API_KEY")
-    llm = Cohere(cohere_api_key = cohere_key)
-
-
-    template = "{user_msg}"
+    
+    llm = ChatCohere(cohere_api_key = cohere_key)
+    template = "Answer the user question briefly - {user_msg}"
     prompt = PromptTemplate.from_template(template)
 
+    chain = prompt | llm.bind_tools([get_current_temperature]) 
 
-    chain = LLMChain(llm=llm, prompt=prompt)
-    response = chain.run(user_msg)
+    response = chain.invoke({"user_msg": user_msg}).content
     return response
+
 
